b5b5e26d2cb32c2bd4af998a02f50257c61e80c2
Ananda Spa <remote.supportvnc@gmail.com>
2018-08-10 17:51:31 -0400

    m

--- ananda.js
+++ ananda.js
@@ -53,6 +53,7 @@ function renderSubmissionDIVs(custRef) {
     // console.log(newISO);
     //let dateISO = custRef.data().createdAt; //.toISOString().slice(0, 10);
     console.log(custRef.get('createdAt'));
+    console.log(firebase.firestore.Timestamp.toDate())
     createdAt.textContent = "dateISO";
     if(custRef.data().grouponCode != "") {
     HearFromUs.textContent = 'G-' + custRef.data().grouponCode
