1681a38ca0ca1c6cd021781ef1362d1078500055
socraticus <40255372+socraticus@users.noreply.github.com>
2018-08-10 00:24:57 -0400

    new Date

--- ananda.js
+++ ananda.js
@@ -160,7 +160,7 @@ facialForm.addEventListener('submit', (e) => {
     // New Data Structure
     // submit captured data to firestore
      db.collection('facialForms').add({
-        date: db.toDate(),
+        date: new Date(),
         hearFromUs: facialForm.HearFromUs.value,
         expChecked: checkedExpCheckbox,
         haveGroupon: checkedGroupon,
