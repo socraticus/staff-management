213ec5e08a84105436e6a5a52ced78c23de4f8ac
Lily's Laptop <remote.supportvnc@gmail.com>
2018-08-14 02:15:36 -0400

    m

--- ananda.js
+++ ananda.js
@@ -136,7 +136,7 @@ function renderSubmissionDIVs(custRef) {
   };
 
 // Initialize Signature Pad
-var canvas = document.querySelector("SignatureCanvas");
+var canvas = document.querySelector("canvas");
 var signaturePad = new SignaturePad(canvas);
 
 // save data to firestore
