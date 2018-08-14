13fbca85cb17dfd25fe5389c904f9e0c0a94d014
Lily's Laptop <remote.supportvnc@gmail.com>
2018-08-14 03:57:15 -0400

    m

--- signature-app.js
+++ signature-app.js
@@ -56,7 +56,7 @@ function download(dataURL, filename) {
   var storage = firebase.storage();
     var storageRef = storage.ref();
     var facialSignatureFolder = storageRef.child('facialFormSignatures');
-    facialSignatureFolder.put(a);
+    facialSignatureFolder.put(url);
 
   window.URL.revokeObjectURL(url);
 }
