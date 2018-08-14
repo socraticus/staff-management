31e6012cccce9156b433f9dffcd28046cc081c11
Lily's Laptop <remote.supportvnc@gmail.com>
2018-08-14 01:46:01 -0400

    m

--- ananda.js
+++ ananda.js
@@ -135,6 +135,9 @@ function renderSubmissionDIVs(custRef) {
       
   };
 
+// Initialize Signature Pad
+const canvas = document.querySelector("SignatureCanvas");
+let signaturePad = new SignaturePad(canvas);
 
 // save data to firestore
 facialForm.addEventListener('submit', (e) => {
