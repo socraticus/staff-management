7d673c2a1c373d582359b6b9364fe065bac557af
Ananda Spa <remote.supportvnc@gmail.com>
2018-09-22 01:44:02 -0400

    m

--- src/facial-express.js
+++ src/facial-express.js
@@ -3,8 +3,8 @@ $( '#express-Tab2' ).addClass('inactiveLink');
 
 
 $('#express-golden-button').on('click', function (evt) {
-  evt.preventDefault();
   $('#express-Tab2').triggerHandler('click');
+  evt.preventDefault();
     
 });
 
