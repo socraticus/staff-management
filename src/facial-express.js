2f604f1270f25528656acdda6b38da7f1c461503
Ananda Spa <remote.supportvnc@gmail.com>
2018-10-14 05:49:18 -0400

    Stripe integration

--- src/facial-express.js
+++ src/facial-express.js
@@ -29,6 +29,78 @@ $('input[name=checkbox], input[name=checkbox-2]').change(function(){
   }
 });
 
+//************//
+//Stripe Implementation
+//************//
+
+//Initialize Stripe
+var stripe = Stripe('pk_test_j5U5yJvpdZW8Jt0HBC7lTMQX');
+var elements = stripe.elements();
+
+// Custom styling can be passed to options when creating an Element.
+var style = {
+  base: {
+    // Add your base input styles here. For example:
+    fontSize: '16px',
+    color: "#32325d",
+  }
+};
+
+// Create an instance of the card Element.
+var card = elements.create('card', {style: style});
+
+// Add an instance of the card Element into the `card-element` <div>.
+card.mount('#card-element');
+
+/*Elements validates user input as it is typed. To help your customers catch mistakes, 
+you should listen to change events on the card Element and display any errors*/
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('express-card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+// Create a token or display an error when the form is submitted.
+var form = document.getElementById('express-shopping-cart');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the customer that there was an error.
+      var errorElement = document.getElementById('express-card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Send the token to your server.
+      stripeTokenHandler(result.token);
+    }
+  });
+});
+
+// Submit the token and the rest of your form to my server
+function stripeTokenHandler(token) {
+  // Insert the token ID into the form so it gets submitted to the server
+  var form = document.getElementById('express-shopping-cart');
+  var hiddenInput = document.createElement('input');
+  hiddenInput.setAttribute('type', 'hidden');
+  hiddenInput.setAttribute('name', 'stripeToken');
+  hiddenInput.setAttribute('value', token.id);
+  form.appendChild(hiddenInput);
+
+  // Submit the form
+  form.submit();
+}
+
+// Create an instance of the card Element.
+var card = elements.create('card', {style: style});
+
+// Add an instance of the card Element into the `card-element` <div>.
+card.mount('#card-element');t
+
+
 // Fix problem with G in Google Reviews
 $('#review-container').find('.romw .romw-source-logo img').css("width", "25px")
 
