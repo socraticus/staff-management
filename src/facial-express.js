447c86ded565fb8d24427cef5354964e31c5752e
Ananda Spa <remote.supportvnc@gmail.com>
2018-10-15 13:58:07 -0400

    m

--- src/facial-express.js
+++ src/facial-express.js
@@ -40,9 +40,18 @@ var elements = stripe.elements();
 // Custom styling can be passed to options when creating an Element.
 var style = {
   base: {
-    // Add your base input styles here. For example:
+    color: '#32325d',
+    lineHeight: '18px',
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: 'antialiased',
     fontSize: '16px',
-    color: "#ffffff",
+    '::placeholder': {
+      color: '#aab7c4'
+    }
+  },
+  invalid: {
+    color: '#fa755a',
+    iconColor: '#fa755a'
   }
 };
 
