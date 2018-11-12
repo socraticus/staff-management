413eb41c42ccbe064991835af73fc0ad1760f57e
Ariel Velazquez <remote.supportvnc@gmail.com>
2018-11-12 18:52:29 -0500

    m

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Headers', '*');
-    if (releaseEvents.method === 'OPTIONS') {
+    if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'GET, PUT,POST, PATCH, DELETE');
         res.status(200).json({});
     }
