d60d41341efbb550d756c7f8e9175adec69818ad
Ariel Velazquez <remote.supportvnc@gmail.com>
2018-11-12 18:46:47 -0500

    m

--- server/index.js
+++ server/index.js
@@ -23,8 +23,11 @@ app.use(bodyParser.urlencoded({
 // CORS Middleware
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', '*');
+    if (releaseEvents.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, PUT,POST, PATCH, DELETE');
+        res.status(200).json({});
+    }
     next();
 });
 
