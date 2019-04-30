3b11bcc44aabc13e4950337e2e7f16eac58fac07
Roilan Armenteros <armenterosroilan@gmail.com>
2019-04-30 13:41:51 -0400

    test

--- server/routes/square.js
+++ server/routes/square.js
@@ -652,8 +652,8 @@ router.get('/parse', function (req, res, next) {
 
 	request('http://api.snapcuba.org/customers.json', (error, response, html) => {
 		if (!error && response.statusCode === 200) {
-			var customers=html;
-			res.send(customers.data);
+			
+			res.send(html.data[0].id);
 		}
 	})
 
