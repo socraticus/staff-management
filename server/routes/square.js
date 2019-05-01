2d7700faca5b029838b7e017d4491e1315806b02
Roilan Armenteros <armenterosroilan@gmail.com>
2019-05-01 09:15:44 -0400

    test

--- server/routes/square.js
+++ server/routes/square.js
@@ -662,7 +662,7 @@ function facialformmigration() {
 			request(options, (error, response, body) => {
 				if (!error && response.statusCode === 200) {
 					 b = JSON.parse(body);
-					console.log(b.app);
+					console.log(b);
 					//var facialfitem = new Facialform(b.app[0]);
 
 					/* facialfitem.save(function (err, facialform) {
