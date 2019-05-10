559dc6658bfb3527416b73074ff4fbc55bb89111
Roilan Armenteros <armenterosroilan@gmail.com>
2019-05-10 16:53:27 -0400

    vue-facial-form

--- src/dev/facial-form-dev.js
+++ src/dev/facial-form-dev.js
@@ -16,24 +16,72 @@ window.onload = function () {
         el: '#vue-facial-form',
         data: {
             title: 'This is a Form Demo',
-            cart: {
-                items: []
+            step: 1,
+            personal: {
+                fullname: "",
+                email: "",
+                phone: "",
+                address: "",
+                city_zip_state: "",
+                Birthdate: ""
             },
-            products: [],
-            discountCode: '',
-            appliedCode: '',
-            discountMessage: '',
-            discountResponse: {
-                message: '',
-                discountAmount: 0,
-                percentage: true
+            health: {
+                wearcontact: false,
+                surgery: false,
+                surgerydescribe: "",
+                skincancer: false,
+                dermatitis: false,
+                keloidscarring: false,
+                acne: false,
+                rosacea: false,
+                broken: false,
+                treatment: false,
+                hypo: false,
+                hyperpig: false,
+                burns: false,
+                anycondition: false,
+                anyconditiondescription: "",
+                allergies: false,
+                latexallergies: false,
+                otherallergies: false,
+                otherallergiesdescription: "",
+                prescription: false,
+                prescriptiondescription: "",
+                pregnant: false,
+                technician: false,
+                techniciandescription: "",
             },
+            skincare: {
+                appointment: "",
+                oftenfacials: "",
+                oftenbody: "",
+                cosmetic: false,
+                finelines: false,
+                wrinkles: false,
+                dull: false,
+                loss: false,
+                dry: false,
+                oily: false,
+                pores: false,
+                redness: false,
+                sensit: false,
+                dark: false,
+                pimples: false,
+                skin: false,
+                other: false,
+            }
         },
         watch: {
 
         },
 
         methods: {
+            prev() {
+                this.step--;
+            },
+            next() {
+                this.step++;
+            },
 
         },
         computed: {
