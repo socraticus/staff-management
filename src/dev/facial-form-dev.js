209c73ae257c2937e7c80c9aca05fdf2e3faa907
Roilan Armenteros <armenterosroilan@gmail.com>
2019-05-14 11:42:52 -0400

    form with vue signature component ready

--- src/dev/facial-form-dev.js
+++ src/dev/facial-form-dev.js
@@ -12,77 +12,10 @@ function testingVue() {
 
 window.onload = function () {
 
-    var canvas = document.getElementById('signature-pad');
-
-    // Adjust canvas coordinate space taking into account pixel ratio,
-    // to make it look crisp on mobile devices.
-    // This also causes canvas to be cleared.
-    function resizeCanvas() {
-        // When zoomed out to less than 100%, for some very strange reason,
-        // some browsers report devicePixelRatio as less than 1
-        // and only part of the canvas is cleared then.
-        var ratio = Math.max(window.devicePixelRatio || 1, 1);
-        canvas.width = canvas.offsetWidth * ratio;
-        canvas.height = canvas.offsetHeight * ratio;
-        canvas.getContext("2d").scale(ratio, ratio);
-    }
-
-    window.onresize = resizeCanvas;
-    resizeCanvas();
-
-    var signaturePad = new SignaturePad(canvas, {
-        backgroundColor: 'rgb(255, 255, 255)' // necessary for saving image as JPEG; can be removed is only saving as PNG or SVG
-    });
-
-    document.getElementById('save-png').addEventListener('click', function () {
-        if (signaturePad.isEmpty()) {
-            return alert("Please provide a signature first.");
-        }
-
-        var data = signaturePad.toDataURL('image/png');
-        console.log(data);
-        window.open(data);
-    });
-
-    document.getElementById('save-jpeg').addEventListener('click', function () {
-        if (signaturePad.isEmpty()) {
-            return alert("Please provide a signature first.");
-        }
-
-        var data = signaturePad.toDataURL('image/jpeg');
-        console.log(data);
-        window.open(data);
-    });
-
-    document.getElementById('save-svg').addEventListener('click', function () {
-        if (signaturePad.isEmpty()) {
-            return alert("Please provide a signature first.");
-        }
-
-        var data = signaturePad.toDataURL('image/svg+xml');
-        console.log(data);
-        console.log(atob(data.split(',')[1]));
-        window.open(data);
-    });
-
-    document.getElementById('clear').addEventListener('click', function () {
-        signaturePad.clear();
-    });
-
-    document.getElementById('draw').addEventListener('click', function () {
-        var ctx = canvas.getContext('2d');
-        console.log(ctx.globalCompositeOperation);
-        ctx.globalCompositeOperation = 'source-over'; // default value
-    });
-
-    document.getElementById('erase').addEventListener('click', function () {
-        var ctx = canvas.getContext('2d');
-        ctx.globalCompositeOperation = 'destination-out';
-    });
 
 
     Vue.component('vue-signature', {
-        template: '#vue-signature',
+        template: '<canvas style="border:1px solid #000000;" :id="uid" class="canvas" :data-uid="uid"></canvas>',
         props: {
             sigOption: {
                 type: Object,
@@ -208,32 +141,6 @@ window.onload = function () {
             });
         }
     })
-    new Vue({
-        data() {
-            return {
-                option: {
-                    penColor: "rgb(0, 0, 0)"
-                }
-            };
-        },
-        methods: {
-            save() {
-                var _this = this;
-                var png = _this.$refs.signature.save()
-                var jpeg = _this.$refs.signature.save('image/jpeg')
-                var svg = _this.$refs.signature.save('image/svg+xml');
-                console.log(png);
-                console.log(jpeg)
-                console.log(svg)
-            },
-            clear() {
-                var _this = this;
-                _this.$refs.signature.clear();
-            }
-        }
-    }).$mount('#app')
-
-
 
 
     var Facialform = new Vue({
@@ -311,7 +218,10 @@ window.onload = function () {
                 skinsensitive: false,
             },
             pictures: false,
-            signature: ""
+            validSignature: "",
+            option: {
+                penColor: "rgb(0, 0, 0)"
+            }
 
         },
         watch: {
@@ -326,7 +236,19 @@ window.onload = function () {
             next() {
                 this.step++;
             },
+            save() {
+                var _this = this;
+                var png = _this.$refs.signature.save()
+                /* var jpeg = _this.$refs.signature.save('image/jpeg')
+                var svg = _this.$refs.signature.save('image/svg+xml'); */
+                validSignature = png;
+                console.log("this is valid signature var: "+validSignature);
 
+            },
+            clear() {
+                var _this = this;
+                _this.$refs.signature.clear();
+            }
         },
         computed: {
 
