ee3afb91c4ba13401c0f48077c2dacd729ac8835
Roilan Armenteros <armenterosroilan@gmail.com>
2019-05-21 21:14:20 -0400

    parents form

--- src/dev/facial-form-dev.js
+++ src/dev/facial-form-dev.js
@@ -144,6 +144,133 @@ window.onload = function () {
             });
         }
     })
+    Vue.component('vue-parentsignature', {
+        template: '<canvas style="border:1px solid #000000;" :id="uid" class="canvas" :data-uid="uid"></canvas>',
+        props: {
+            sigOption: {
+                type: Object,
+                default: () => {
+                    return {
+                        backgroundColor: 'rgb(255,255,255)',
+                        penColor: 'rgb(0, 0, 0)'
+                    }
+                },
+            },
+            w: {
+                type: String,
+                default: "100%"
+            },
+            h: {
+                type: String,
+                default: "100%"
+            },
+            clearOnResize: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data() {
+            return {
+                sig: () => { },
+                option: {
+                    backgroundColor: 'rgb(255,255,255)',
+                    penColor: 'rgb(0, 0, 0)'
+                },
+                uid: ""
+            }
+        },
+        created() {
+            var _this = this;
+            this.uid = "canvas" + _this._uid
+            var sigOptions = Object.keys(_this.sigOption);
+            for (var item of sigOptions) {
+                _this.option[item] = _this.sigOption[item]
+            }
+        },
+        methods: {
+            draw() {
+                var _this = this;
+                var canvas = document.getElementById(_this.uid)
+                _this.sig = new SignaturePad(canvas, _this.option);
+                function resizeCanvas() {
+                    var url;
+                    if (!_this.isEmpty()) {
+                        url = _this.save();
+                    }
+                    var ratio = Math.max(window.devicePixelRatio || 1, 1);
+                    canvas.width = canvas.offsetWidth * ratio;
+                    canvas.height = canvas.offsetHeight * ratio;
+                    canvas.getContext("2d").scale(ratio, ratio);
+                    _this.clear();
+                    !_this.clearOnResize && url !== undefined && _this.fromDataURL(url)
+                }
+                window.addEventListener("resize", resizeCanvas);
+                resizeCanvas();
+            },
+            clear() {
+                var _this = this;
+                _this.sig.clear();
+            },
+            save(format) {
+                var _this = this;
+                return format ? _this.sig.toDataURL(format) : _this.sig.toDataURL()
+                // signaturePad.toDataURL(); // save image as PNG
+                // signaturePad.toDataURL("image/jpeg"); // save image as JPEG
+                // signaturePad.toDataURL("image/svg+xml"); // save image as SVG
+            },
+            fromDataURL(url) {
+                var _this = this;
+                _this.sig.fromDataURL(url)
+            },
+            isEmpty() {
+                var _this = this;
+                return _this.sig.isEmpty();
+            },
+            undo() {
+                var _this = this;
+                var data = _this.sig.toData();
+                if (data) {
+                    data.pop()
+                    _this.sig.fromData(data);
+                }
+            },
+            addWaterMark(data) {
+                var _this = this;
+                if (!(Object.prototype.toString.call(data) == '[object Object]')) {
+                    throw new Error("Expected Object, got " + typeof data + ".")
+                } else {
+                    var vCanvas = document.getElementById(_this.uid);
+                    var textData = {
+                        text: data.text || '',
+                        x: data.x || 20,
+                        y: data.y || 20,
+                        sx: data.sx || 40,
+                        sy: data.sy || 40
+                    }
+
+                    var ctx = vCanvas.getContext('2d');
+                    ctx.font = data.font || '20px sans-serif';
+                    ctx.fillStyle = data.fillStyle || "#333";
+                    ctx.strokeStyle = data.strokeStyle || "#333";
+                    if (data.style == 'all') {
+                        ctx.fillText(textData.text, textData.x, textData.y);
+                        ctx.strokeText(textData.text, textData.sx, textData.sx);
+                    } else if (data.style == 'stroke') {
+                        ctx.strokeText(textData.text, textData.sx, textData.sx);
+                    } else {
+                        ctx.fillText(textData.text, textData.x, textData.y);
+                    }
+                    _this.sig._isEmpty = false
+                }
+            }
+        },
+        mounted() {
+            var _this = this;
+            this.$nextTick(() => {
+                _this.draw()
+            });
+        }
+    })
 
     Vue.component('date-picker', {
         template: '<input/>',
@@ -153,7 +280,7 @@ window.onload = function () {
             $(this.$el).datepicker({
                 //dateFormat: this.dateFormat,
                 language: 'en',
-                //minDate: new Date(), // Now can select only dates, which goes after today
+                view: 'years',
                 onSelect: function (date) {
                     self.$emit('update-date', date);
                 }
@@ -257,19 +384,25 @@ window.onload = function () {
                 scar: false,
                 skinsensitive: false,
             },
+            Minor: {
+                parentName: "",
+                parentSignature: ""
+            },
             Errors: {
                 fullName: false,
                 phone: false,
                 email: false,
                 address: false,
                 city_zip_state: false,
-                Birthdate: false
+                Birthdate: false,
+                parentName: false
             },
             pictures: false,
             signature: "",
             option: {
                 penColor: "rgb(0, 0, 0)"
             },
+            parentsForm: false,
             submitbtn: "SUBMIT"
 
         },
@@ -301,14 +434,44 @@ window.onload = function () {
                 var _this = this;
                 _this.$refs.signature.clear();
             },
+            saveparent() {
+                var _this = this;
+                var png = _this.$refs.parentsignature.save()
+                /* var jpeg = _this.$refs.signature.save('image/jpeg')
+                var svg = _this.$refs.signature.save('image/svg+xml'); */
+                signature = png;
+                console.log("this is valid signature var: " + signature);
+                return png;
+            },
+            clearparent() {
+                var _this = this;
+                _this.$refs.parentsignature.clear();
+            },
             updateDate: function (date) {
                 this.personal.Birthdate = date;
+                var age = this.calculateAge(date);
+                if (age < 18) {
+                    this.parentsForm = true;
+                }
+                else {
+                    this.parentsForm = false;
+                }
             },
             updatePhone: function (phone) {
                 this.personal.phone = phone;
             },
             updateSubmitbtn(submitbtn) {
                 this.submitbtn = submitbtn;
+            },
+            calculateAge(dateString) {
+                var today = new Date();
+                var birthDate = new Date(dateString);
+                var age = today.getFullYear() - birthDate.getFullYear();
+                var m = today.getMonth() - birthDate.getMonth();
+                if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+                    age--;
+                }
+                return age;
             },
             validation() {
                 var errors = 0;
@@ -334,6 +497,13 @@ window.onload = function () {
                     errors++;
                 } else {
                     this.Errors.Birthdate = false;
+
+                }
+                if (this.Minor.parentName == "" && this.parentsForm == true) {
+                    this.Errors.parentName = true;
+                    errors++;
+                } else {
+                    this.Errors.parentName = false;
 
                 }
                 if (this.personal.address == "") {
