f0ef3c6b1f34eb7ba001943dd409d90982c08d22
Roilan Armenteros <armenterosroilan@gmail.com>
2019-05-24 11:07:30 -0400

    test

--- server/routes/facial.js
+++ server/routes/facial.js
@@ -102,7 +102,7 @@ function intakeformMigration() {
                 console.log(" saved to intakeitem collection.");
             });
         }); */
-        Facialform.findOne({ email: emails[0], fullname: { $ne: [] } },'fullname', function (err, resad) {
+        Facialform.findOne({ email: emails[0], fullname: { $ne: [] } },{projection:{ _id: 0 }, function (err, resad) {
 
             console.log(resad);
             var intakeitem = new Intakeform(resad);
